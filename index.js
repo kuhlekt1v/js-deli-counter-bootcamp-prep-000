bad836e28380189145460840696824067b85f636
Cody Sheridan <csheridan994@gmail.com>
2018-04-27 01:58:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, name){
 } 
 
 function nowServing(line){
-  if (line === 0) {
+  if (line.length === 0) {
     return "There is nobody waiting to be served!"
   }
   else {
