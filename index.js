cb335873e31718d8b3b2b06e8e6007fd10ad4586
Cody Sheridan <csheridan994@gmail.com>
2018-04-27 02:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(line, name){
 
 function nowServing(line){
   if (line.length === 0) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
   else {
     //access index 0 (first person in line) and remove it. 
