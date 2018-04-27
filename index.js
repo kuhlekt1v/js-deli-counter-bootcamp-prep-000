4c0ad34994b83399722a1930e305eae4f2182ebe
Cody Sheridan <csheridan994@gmail.com>
2018-04-27 01:47:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function takeANumber(line, name){
+  line.push(name);
+  var position = line.length;
+  return "Welcome, " + name + ". You are number " + position + " in line.";
+} 
+
+function nowServing(line){
+ for (let i = 0; i < line.length; --i;)
+}
+
